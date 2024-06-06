a91280a4b0ea88b33d3a770b2b1a296a71dd1aef
martinaliporata <martinaliporata@gmail.com>
2024-06-06 16:32:13 +0200

    correct

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-createApp({
-    data() {
+const {createApp} = Vue
+
+
+createApp ({
+    data(){
         return {
             arrayEmail: [],
         }
     },
     methods: {
-        generateEmail: function(){
+        getData(){
             for (let index=0; index < 10; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) =>{
-                    let email = risultato.data.response;
-                    this.arrayEmail.push(email);
-                })
+                    console.log(response.data.response);
+                    this.arrayEmail.push(response.data.response);
+                });
             }
         }
     },
-    created () {
-        this.generateEmail
+    created(){
+        this.getData()
     }
-})
+}).mount('#app')
