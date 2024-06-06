f66701ac66712193198853496aada74f4f8bdbea
martinaliporata <martinaliporata@gmail.com>
2024-06-06 08:41:31 +0200

    added then, push and life cycle hook

--- js/script.js
+++ js/script.js
@@ -8,9 +8,14 @@ createApp({
         generateEmail: function(){
             for (let index=0; index < 10; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                
+                .then((response) =>{
+                    let email = risultato.data.response;
+                    this.arrayEmail.push(email);
+                })
             }
         }
     },
-    
+    created () {
+        this.generateEmail
+    }
 })
